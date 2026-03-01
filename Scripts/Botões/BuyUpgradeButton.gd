dc6c0a532249fbe177de1ee8b2759c2a31b814c4
NormyNaoExiste <felipedaviam@gmail.com>
2026-03-01 13:15:02 -0300

    Upgrade Data adicionado

--- Scripts/Botões/BuyUpgradeButton.gd
+++ Scripts/Botões/BuyUpgradeButton.gd
@@ -1,6 +1,8 @@
 extends Button
 
 @onready var shop_dialogue: Label = $"../../ShopDialogue"
+@onready var shop_upgrade_slot_1: Panel = $".."
+
 const DIALOGUE_DEFAULT: String = "Dialogue box for upgrades"
 
 func _ready() -> void:
@@ -8,7 +10,8 @@ func _ready() -> void:
 	mouse_exited.connect(_on_mouse_exited)
 
 func _on_mouse_entered():
-	shop_dialogue.text = "Buy Upgrade"
-
+	shop_upgrade_slot_1._on_mouse_entered()
+	shop_dialogue.text = str("Buy ", shop_upgrade_slot_1.upgrade_text)
 func _on_mouse_exited():
 	shop_dialogue.text = DIALOGUE_DEFAULT
+	shop_upgrade_slot_1._on_mouse_exited()
---
+++ Scripts/Sistemas/UpgradeData.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name  UpgradeData
+
+#Propriedades
+var nome: String = "NewUpgrade"
+var descricao: String = "Descrição do upgrade"
+var custo_base: int = 10
+var modificador: int = 1
+var tipo: String = "clique"
+
+#Muda com a Gameplay
+var nivel: int = 0
